26a130380298236842e24b9d225cad6e7f9ad019
WailedParsley36 <71410855+WailedParsley36@users.noreply.github.com>
2022-11-08 15:54:47 +0100

    - Final push

--- TacticalReload/Game/Player.cs
+++ TacticalReload/Game/Player.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
+using System.Xml.Linq;
 using TacticalReload.Pages;
 using WpfAnimatedGif;
 
@@ -16,6 +18,7 @@ namespace TacticalReload.Game
         private readonly ReloaderGame _Game;
 
         private ImageAnimationController _GunController;
+        private Image _Gun;
         private ProgressBar _HealthBar;
         private Label _PlayerName;
 
@@ -24,6 +27,10 @@ namespace TacticalReload.Game
         private ProgressBar _CooldownBar;
         private Border _CooldownElement;
 
+        private Border _ReloadElement;
+        private DockPanel _ReloadMarker;
+        private DockPanel _ReloadRanger;
+
         public Player(ReloaderGame game, Key actionKey, string name, bool second)
         {
             ActionKey = actionKey;
@@ -31,19 +38,35 @@ namespace TacticalReload.Game
 
             if (second)
             {
-                _PlayerName = _Game.BarDescription;
+                _Gun = _Game.GunAnimation2;
+                _PlayerName = _Game.PlayerName2;
+                _HealthBar = _Game.PlayerHealth2;
+
+                _BarDescription = _Game.BarDescription2;
+                _CooldownBar = _Game.CooldownBar2;
+                _CooldownElement = _Game.CooldownElement2;
+
+                _ReloadElement = _Game.ReloadElement2;
+                _ReloadMarker = _Game.ReloadMarker2;
+                _ReloadRanger = _Game.ReloadRange2;
             }
             else
             {
+                _Gun = _Game.GunAnimation;
                 _PlayerName = _Game.PlayerName;
                 _HealthBar = _Game.PlayerHealth;
 
                 _BarDescription = _Game.BarDescription;
                 _CooldownBar = _Game.CooldownBar;
                 _CooldownElement = _Game.CooldownElement;
+
+                _ReloadElement = _Game.ReloadElement;
+                _ReloadMarker = _Game.ReloadMarker;
+                _ReloadRanger = _Game.ReloadRange;
             }
 
             Name = name;
+            Health = 100;
         }
         public void LoadController(ImageAnimationController gun)
         {
@@ -75,67 +98,178 @@ namespace TacticalReload.Game
             }
         }
         public Key ActionKey { get; private set; } = Key.R;
+        public int ReloadCombo { get; private set; } = 100;
+        public bool IsShooting { get; private set; } = false;
 
+        private bool _Paused;
 
         public void Pause(bool unpause = false)
         {
+            _Paused = !unpause;
             _Game.Dispatcher.Invoke(() =>
             {
-                if (unpause)
+                if (unpause && IsShooting)
                     _GunController.Play();
                 else
                     _GunController.Pause();
             });
         }
 
-        public void Shoot(double speedMultipler)
+        private (int, int) _ReloadRange;
+        private int _ReloadValue;
+        private DispatcherTimer? _ReloadTimer;
+        public void Reload()
         {
-            var timer = new DispatcherTimer(DispatcherPriority.Normal, _Game.Dispatcher);
-            timer.Tick += (sender, args) =>
+            _ReloadTimer?.Stop();
+            _ReloadTimer = new DispatcherTimer(DispatcherPriority.Normal, _Game.Dispatcher);
+
+            _ReloadValue = 0;
+            _ReloadRange.Item1 = RandomNumberGenerator.GetInt32(96);
+            _ReloadRange.Item2 = Math.Clamp((_ReloadRange.Item1 + RandomNumberGenerator.GetInt32(50)), 5, 100);
+            IsShooting = true;
+
+            _ReloadTimer.Tick += (sender, args) =>
             {
-                
+                if (_Paused)
+                    return;
+
+                _ReloadValue += /*(int)((ReloadCombo / 100) * 1) */1;
+                MoveReloadMarkTo(_ReloadValue);
+
+                if (_GunController.CurrentFrame == 34)
+                {
+                    _GunController.Pause();
+                    _GunController.GotoFrame(9);
+                    ImageBehavior.SetAnimationDuration(_Gun, new System.Windows.Duration(new TimeSpan(0, 0, 5)));
+                    _GunController.Play();
+                }
+
+                if (_ReloadValue == 100)
+                    Cooldown(RandomNumberGenerator.GetInt32(11) / 10);
+                else if (_ReloadValue >= _ReloadRange.Item1 && _ReloadValue <= _ReloadRange.Item2)
+                {
+                    //TODO: Highlight marker
+                    _BarDescription.Content = "Shoot already!";
+                }
+                else
+                {
+                    _BarDescription.Content = $"Reloading...{_ReloadRange.Item1}-{_ReloadRange.Item2}";
+                }
             };
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            _ReloadTimer.Interval = new TimeSpan(0, 0, 0, 0, Math.Clamp(RandomNumberGenerator.GetInt32(101), 10, 100));
 
             _Game.Dispatcher.Invoke(() =>
             {
+                _CooldownElement.Visibility = System.Windows.Visibility.Collapsed;
+                _ReloadElement.Visibility = System.Windows.Visibility.Visible;
+                _ReloadMarker.Visibility = System.Windows.Visibility.Visible;
+                _ReloadRanger.Visibility = System.Windows.Visibility.Visible;
 
-                timer.Start();
+                _BarDescription.Content = $"Reloading...{_ReloadRange.Item1}-{_ReloadRange.Item2}";
+
+                _GunController.Pause();
+                _GunController.GotoFrame(9);
+                ImageBehavior.SetAnimationDuration(_Gun, new System.Windows.Duration(new TimeSpan(0, 0, 5)));
+                _GunController.Play();
+
+                SetReloadRangeUI();
+                MoveReloadMarkTo(0);
+
+                _ReloadTimer.Start();
             });
-            //TODO: Shoot here
         }
-
-        public void Cooldown(double ammount)
+        private void SetReloadRangeUI()
         {
-            var timer = new DispatcherTimer(DispatcherPriority.Normal, _Game.Dispatcher);
-            timer.Tick += (sender, args) =>
+            double left = 80 + 475 * ((double)_ReloadRange.Item1 / 100);
+            double right = 80 + 475 * ((100 - (double)_ReloadRange.Item2) / 100);
+            _ReloadRanger.Margin = new System.Windows.Thickness(left, -100, right, _ReloadMarker.Margin.Bottom);
+        }
+        private void MoveReloadMarkTo(int percent)
+        {
+            _ReloadMarker.Margin = new System.Windows.Thickness(930 * ((double)percent / 100) - 465, _ReloadMarker.Margin.Top, _ReloadMarker.Margin.Right, _ReloadMarker.Margin.Bottom);
+        }
+        public void Cooldown(double ammount = 1, bool shot = false)
+        {
+            _ReloadTimer?.Stop();
+            _ReloadTimer = new DispatcherTimer(DispatcherPriority.Normal, _Game.Dispatcher);
+            ammount = (double)RandomNumberGenerator.GetInt32(15, 101) / 100;
+            _ReloadTimer.Tick += (sender, args) =>
             {
+                if (_Paused)
+                    return;
+
                 _CooldownBar.Value -= ammount;
+
+                if (_GunController.CurrentFrame == 10)
+                {
+                    _GunController.Pause();
+                    _GunController.GotoFrame(5);
+                    ImageBehavior.SetAnimationDuration(_Gun, new System.Windows.Duration(new TimeSpan(0, 0, 12)));
+                    _GunController.Play();
+                }
+
+                if (_CooldownBar.Value <= 0)
+                {
+                    _CooldownElement.Visibility = System.Windows.Visibility.Collapsed;
+                    Reload();
+                }
             };
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            _ReloadTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+            ReloadCombo = 100;
+            _ReloadValue = 0;
+            _ReloadRange = (0, 0);
+            IsShooting = false;
 
             _Game.Dispatcher.Invoke(() =>
             {
                 //After shooting
-                _GunController.GotoFrame(3);
+                _GunController.Pause();
+                _GunController.GotoFrame(shot ? 0 : 5);
+                ImageBehavior.SetAnimationDuration(_Gun, new System.Windows.Duration(new TimeSpan(0, 0, 12)));
+                _GunController.Play();
 
                 _CooldownElement.Visibility = System.Windows.Visibility.Visible;
-                //TODO: Gun visibility
+                _ReloadElement.Visibility = System.Windows.Visibility.Collapsed;
+                _ReloadMarker.Visibility = System.Windows.Visibility.Collapsed;
+                _ReloadRanger.Visibility = System.Windows.Visibility.Collapsed;
 
                 _BarDescription.Content = "Cool it down!";
 
                 _CooldownBar.Value = 100;
-                timer.Start();
+                _ReloadTimer.Start();
             });
         }
 
+        public void Shoot(double damageMultiplier)
+        {
+            if (!IsShooting)
+                return;
+
+            if (_ReloadValue >= _ReloadRange.Item1 && _ReloadValue <= _ReloadRange.Item2)
+            {
+                _GunController.GotoFrame(1);
+                ImageBehavior.SetAnimationDuration(_Gun, new System.Windows.Duration(new TimeSpan(0, 0, 12)));
+                _GunController.Play();
+                IsShooting = false;
+                ReloadCombo++;
+                if (_Game.PlayerName2 == _PlayerName)
+                    _Game.Players[0].TakeDamage(RandomNumberGenerator.GetInt32((int)(damageMultiplier * 10) + (_ReloadValue / 10)), RandomNumberGenerator.GetInt32(0, 2) == 0 ? false : true);
+                else
+                    _Game.Players[1].TakeDamage(RandomNumberGenerator.GetInt32((int)(damageMultiplier * 10) + (_ReloadValue / 10)), RandomNumberGenerator.GetInt32(0, 2) == 0 ? false : true);
+            }
+
+            Cooldown(RandomNumberGenerator.GetInt32(11) / 10);
+        }
         public void TakeDamage(int ammount, bool crit = false)
         {
+            if (crit)
+                ammount = (int)(ammount * 1.25);
+
             Health -= ammount;
 
-            if (crit)
-                Console.WriteLine("Critical Hit!");
+            if (Health == 0)
+                _Game.Win(this);
         }
-
     }
 }
--- TacticalReload/Pages/ReloaderGame.xaml.cs
+++ TacticalReload/Pages/ReloaderGame.xaml.cs
@@ -34,6 +34,17 @@ namespace TacticalReload.Pages
             ImageBehavior.AddAnimationLoadedHandler(GunAnimation, (sender, args) =>
             {
                 Players[0].LoadController(ImageBehavior.GetAnimationController(GunAnimation));
+
+                if (!IsStarted)
+                    StartGame();
+            });
+
+            ImageBehavior.AddAnimationLoadedHandler(GunAnimation2, (sender, args) =>
+            {
+                Players[1].LoadController(ImageBehavior.GetAnimationController(GunAnimation2));
+
+                if (!IsStarted)
+                    StartGame();
             });
 
             Players = new[] { new Player(this, Key.R, "Player 1", false), new Player(this, Key.NumPad5, "Player 2", true) };
@@ -41,66 +52,127 @@ namespace TacticalReload.Pages
 
         public Player[] Players { get; private set; }
         public bool Paused { get; private set; } = false;
+        public bool IsStarted = false;
 
         private DispatcherTimer PauseTimer = new DispatcherTimer();
 
+        public void Win(Player player)
+        {
+            PauseTimer.Stop();
+
+            PauseScreen.Visibility = Visibility.Visible;
+            Paused = true;
+
+            Players[0].Pause();
+            Players[1].Pause();
+
+            PauseTimer = new DispatcherTimer(DispatcherPriority.Normal, Dispatcher);
+            int times = 5;
+            PauseDescription.Content = $"{(Players[0] == player ? Players[1].Name : Players[0].Name)} has win!";
+            PauseTimer.Tick += (sender, args) =>
+            {
+                times--;
+                if (times == 0)
+                {
+                    PauseTimer.Stop();
+
+                    Core.Window.LoadPage<MainMenu>();
+                }
+            };
+            PauseTimer.Interval = new TimeSpan(0, 0, 1);
+            PauseTimer.Start();
+        }
+        private void StartGame()
+        {
+            IsStarted = true;
+            PauseTimer.Stop();
+
+            PauseScreen.Visibility = Visibility.Visible;
+            Paused = true;
+
+            PauseTimer = new DispatcherTimer(DispatcherPriority.Normal, Dispatcher);
+            int times = 5;
+            PauseDescription.Content = "Starting in " + times;
+            PauseTimer.Tick += (sender, args) =>
+            {
+                times--;
+                PauseDescription.Content = "Starting in " + times;
+                if (times == 0)
+                {
+                    PauseScreen.Visibility = Visibility.Collapsed;
+                    Paused = false;
+
+                    PauseTimer.Stop();
+
+                    Players[0].Reload();
+                    Players[1].Reload();
+                }
+            };
+            PauseTimer.Interval = new TimeSpan(0, 0, 1);
+            PauseTimer.Start();
+        }
+        private void Pause()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (Paused)
+                {
+                    PauseScreen.Visibility = Visibility.Visible;
+                    PauseDescription.Content = "Game Paused";
+                }
+                else
+                {
+                    PauseTimer.Stop();
+
+                    Paused = true;
+                    PauseScreen.Visibility = Visibility.Visible;
+
+                    PauseTimer = new DispatcherTimer(DispatcherPriority.Normal, Dispatcher);
+                    int times = 5;
+                    PauseDescription.Content = "Starting in " + times;
+                    PauseTimer.Tick += (sender, args) =>
+                    {
+                        times--;
+                        PauseDescription.Content = "Starting in " + times;
+                        if (times == 0)
+                        {
+                            PauseScreen.Visibility = Visibility.Collapsed;
+                            Paused = false;
+
+                            Players[0].Pause(!Paused);
+                            Players[1].Pause(!Paused);
+
+                            PauseTimer.Stop();
+                        }
+                    };
+                    PauseTimer.Interval = new TimeSpan(0, 0, 1);
+                    PauseTimer.Start();
+                }
+
+                Players[0].Pause(!Paused);
+                Players[1].Pause(!Paused);
+            });
+        }
         public void KeyPressed(Key key)
         {
             if (key == Key.P || key == Key.Escape)
             {
                 //Pausing game
                 Paused = !Paused;
-
-                Dispatcher.Invoke(() =>
-                {
-                    if (Paused)
-                    {
-                        PauseScreen.Visibility = Visibility.Visible;
-                        PauseDescription.Content = "Game Paused";
-                    }
-                    else
-                    {
-                        PauseTimer.Stop();
-
-                        Paused = true;
-                        
-                        PauseTimer = new DispatcherTimer(DispatcherPriority.Normal, Dispatcher);
-                        int times = 5;
-                        PauseDescription.Content = "Starting in " + times;
-                        PauseTimer.Tick += (sender, args) =>
-                        {
-                            times--;
-                            PauseDescription.Content = "Starting in " + times;
-                            if (times == 0)
-                            {
-                                PauseScreen.Visibility = Visibility.Collapsed;
-                                Paused = false;
-
-                                Players[0].Pause(!Paused);
-                                //Players[1].Pause(rPause);
-
-                                PauseTimer.Stop();
-                            }
-                        };
-                        PauseTimer.Interval = new TimeSpan(0, 0, 1);
-                        PauseTimer.Start();
-                    }
-
-                    Players[0].Pause(!Paused);
-                    //Players[1].Pause(rPause);
-                });
+                Pause();
             }
 
             if (key == Players[0].ActionKey)
-                Players[0].Shoot(1.25);
-        }
-
-        private void DealDamage(bool second)
-        {
-            Dispatcher.Invoke(() =>
             {
+                if (Players[0].IsShooting)
+                    Players[0].Shoot(1);
+            }
 
-            });
+            if (key == Players[1].ActionKey)
+            {
+                if (Players[1].IsShooting)
+                    Players[1].Shoot(1);
+            }
         }
     }
 }
